f96e6f94d092be16148ca68b7dd70fdd26da8096
Yusuke Katsuragawa <hakodate.katsuragawa.yusuke@gmail.com>
2021-09-19 09:00:33 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.amber),
-      home: TodoApp()
-    );
+        theme: ThemeData(primarySwatch: Colors.amber), home: TodoApp());
   }
 }
